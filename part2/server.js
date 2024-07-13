70e98c754ba1c5faa7eb42b4344e27ed53be3780
“lest”-xu <autodataxx@gmail.com>
2024-07-13 11:17:45 -0700

    update

--- part2/server.js
+++ part2/server.js
@@ -8,8 +8,7 @@ app.use(bodyParser.json());
 // setup handlebars view engine
 const handlebars = require('express-handlebars');
 
-app.engine('handlebars', 
-	handlebars({defaultLayout: 'main'}));
+app.engine('handlebars', handlebars({ defaultLayout: 'main' }));
 
 app.set('view engine', 'handlebars');
 
@@ -21,43 +20,83 @@ const cities = require('./zipCodeModule_v2');
 
 // GET request to the homepage
 
-app.get('/',  (req, res) => {
+app.get('/', (req, res) => {
 	res.render('homeView');
 });
 
+// GET /zip
 app.get('/zip', (req, res) => {
-	
-
+	// get zip from query url
+	const zip = req.query.id;
+	// check if id exist
+	if (zip) {
+		// get result by zip
+		const result = cities.lookupByZipCode(zip);
+		const data = { data: result };
+		res.render('lookupByZipView', data); // render view
+	} else {
+		res.render('lookupByZipForm'); // no zip present render lookupByZipForm
+	}
 });
 
+// POST /zip
 app.post('/zip', (req, res) => {
-	
-
+	// get zip from form's request body
+	const zip = req.body.id;
+	// get result by zip
+	const result = cities.lookupByZipCode(zip);
+	const data = { data: result };
+	res.render('lookupByZipView', data); // render view
 });
 
 // Implement the JSON, XML, & HTML formats
 
 app.get('/zip/:id', (req, res) => {
-	
+	// get zip param
+	const zip = req.params.id;
+	console.log('zip2', zip);
+	// get result by zip
+	const result = cities.lookupByZipCode(zip);
+	const data = { data: result };
+
+	// check the header JSON, XML, & HTML formats
+	const header = req.headers.accept;
+	console.log('header', header);
+	if (header.includes('application/json')) {
+		res.json(result); // send json result
+	} else if (header === 'application/xml') {
+		// define the xml format
+		const xml = `
+			<zip>${result._id}</zip>
+			<city>${result.city}</city>
+			<state>${result.state}</state>
+			<pop>${result.pop}</pop>
+		`;
+		res.type('application/xml');
+		res.send(xml); // send xml result
+	} else {
+		console.log('reading the zip view');
+		res.render('lookupByZipView', data); // render view
+	}
 
 });
 
 
 
 app.get('/city', (req, res) => {
-	
-	
+
+
 });
 
 app.post('/city', (req, res) => {
-	
+
 
 });
 
 // Implement the JSON, XML, & HTML formats
 
 app.get('/city/:city/state/:state', (req, res) => {
-	
+
 
 
 });
@@ -65,14 +104,14 @@ app.get('/city/:city/state/:state', (req, res) => {
 
 
 app.get('/pop', (req, res) => {
-	
-	
+
+
 });
 
 // Implement the JSON, XML, & HTML formats
 
 app.get('/pop/:state', (req, res) => {
-	
+
 
 });
 
@@ -83,7 +122,7 @@ app.use((req, res) => {
 });
 
 app.listen(3000, () => {
-  console.log('http://localhost:3000');
+	console.log('http://localhost:3000');
 });
 
 
--- part2/zipCodeModule_v2.js
+++ part2/zipCodeModule_v2.js
@@ -1,14 +1,50 @@
 let data = require('./zips.json');
 
 module.exports.lookupByZipCode =  (zip) => {
-		
+    // print info
+    // console.log(`Look up by zip code (${zip})`);
+    // create result obejct
+    const result = data.find(item => item._id === zip);
+    // print result and return
+    // console.log(result);
+    return result;
 };
 
 module.exports.lookupByCityState = (city, state) => {
-	
+    // print info
+    // console.log(`Look up by city (${city}, ${state})`);
+    // create the result obejct for return
+    let result = {
+        'city': city,
+        'state': state,
+        'data': []
+    }
+    // filter by city and state
+    const filtered = data.filter(i => i.city === city && i.state === state).map(i => ({zip: i._id, pop: i.pop}));
+    // assign filtered data to result
+    result.data = filtered;
+
+    // print the result and return
+    // console.log(result);
+    return result;
 };
 
 module.exports.getPopulationByState = (state) => {
-	
+    // print info
+    // console.log(`Get population by state (${state})`);
+    // get total pop by state
+    const totalPop = data.reduce((pop, item) => {
+        if (item.state === state) {
+            return pop + item.pop;
+        }
+        return pop;
+    }, 0);
+    // create the result obejct for return
+    const result = {
+        'state': state,
+        'pop': totalPop
+    };
+    // print the result and return
+    // console.log(result);
+    return result;
 };
-
